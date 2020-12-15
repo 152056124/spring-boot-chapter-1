bc8fb23d433b961ac9298aa3128f8d12dd04d5ca
zhangtc <zhangtc@ajiot.net>
2020-12-15 09:06:32 +0800

    0.2 swagger配置

--- src/main/java/com/example/demo/controller/ConfigController.java
+++ src/main/java/com/example/demo/controller/ConfigController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.config.CollectionConfig;
 import com.example.demo.config.MyConfig;
 import com.example.demo.config.RandomConfig;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api("配置文件接口")
 @RestController
 @RequestMapping("/config")
 public class ConfigController {
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,9 +1,16 @@
 package com.example.demo.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+@ApiModel(description = "用户实体")
 @Data
 public class User {
-    private String username;
-    private String password;
+    @ApiModelProperty("用户编号")
+    private Long id;
+    @ApiModelProperty("用户姓名")
+    private String name;
+    @ApiModelProperty("用户年龄")
+    private Integer age;
 }
